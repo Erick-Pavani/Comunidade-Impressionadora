9b3654947c25dc0a15076def6c4488d8d612f266
Erick-Pavani <erickspavanii@gmail.com>
2022-05-18 23:02:12 -0300

    Added a navbar

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 # Imports
 from flask import Flask, render_template
 
-# Flask Setup
+#------------------------------ Flask Setup ------------------------
 app = Flask(__name__)
 
-# Flask Routes
+lista_usuarios = ['Erick', 'Gabi', 'Gabriele', 'Sandra', 'Pingo']
+
+#------------------------------ Flask Routes -----------------------
+
+# Home
 @app.route('/')
 def home():
     return render_template('home.html')
 
-
+# Contato
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+# Contato
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios = lista_usuarios)
 
 if __name__== '__main__':
     app.run(debug=True)
