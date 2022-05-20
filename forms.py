7e6a60c5b2b21ecdda13c3929a862bb319f81787
Erick-Pavani <erickspavanii@gmail.com>
2022-05-19 21:50:40 -0300

    Added login and register forms

---
+++ forms.py
@@ -0,0 +1,21 @@
+# Imports
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+#-------------------- Classes----------------------
+
+# Login
+class FormLogin(FlaskForm):
+    email = StringField('E-mail', validators = [DataRequired(), Email()])
+    senha = PasswordField('Senha', validators = [DataRequired(), Length(6, 20)])
+    lembrar_dados = BooleanField('Quero permanecer conectado!')
+    botao_login = SubmitField('Fazer Login')
+
+# Criar Conta
+class FormCriarConta(FlaskForm):
+    username = StringField('Nome do usuário', validators = [DataRequired()])
+    email = StringField('E-mail', validators = [DataRequired(), Email()])
+    senha = PasswordField('Senha', validators = [DataRequired(), Length(6, 20)])
+    confirmacao = PasswordField('Confirme a senha', validators = [DataRequired(), EqualTo('senha')])
+    botao_criar_conta = SubmitField('Criar conta')
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # Imports
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import FormLogin, FormCriarConta
 
 #------------------------------ Flask Setup ------------------------
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '4c673c0ef7953896bc739e5c6161b2ad'
+
+#-------------------------------------------------------------------
 
 lista_usuarios = ['Erick', 'Gabi', 'Gabriele', 'Sandra', 'Pingo']
 
@@ -22,6 +27,13 @@ def contato():
 @app.route('/usuarios')
 def usuarios():
     return render_template('usuarios.html', lista_usuarios = lista_usuarios)
+
+# Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form_login = FormLogin()
+    form_criar_conta = FormCriarConta()
+    return render_template('login.html', form_login = form_login, form_criar_conta = form_criar_conta)
 
 if __name__== '__main__':
     app.run(debug=True)
